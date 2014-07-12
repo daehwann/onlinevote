a510fc788b6d6655ec33c50ea51330a1b1b5d0c8
daehwan <daehwan864@gmail.com>
2014-07-12 23:38:16 +0900

    vote 클래스 생성

---
+++ src/main/java/com/daehwan/vote/Vote.java
@@ -0,0 +1,30 @@
+package com.daehwan.vote;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Vote {
+
+	public static Set<Opinion> set = new HashSet<Opinion>();
+	public static void main(String[] args) {
+		Opinion o1 = new Opinion();
+		o1.setIp("111.222.333.444");
+		o1.setOpinion(true);
+		
+		Opinion o2 = new Opinion();
+		o2.setIp("111.222.333.444");
+		o2.setOpinion(true);
+		
+		set.add(o1);
+		set.add(o2);
+		
+		Iterator<Opinion> itr = set.iterator();
+		while(itr.hasNext()) {
+			Opinion o = itr.next();
+			System.out.println(o.getIp());
+		}
+		
+	}
+	
+}
